8a2f842de5acb7ec8ccba16f3229500987dd9a8d
Leandro L. Lobo <148393517+Leandrollobo@users.noreply.github.com>
2024-01-30 19:17:41 -0300

    Update 3-PostgreSQL - Procedure levantamento diario.sql
    inserted the commented code.

--- 3-PostgreSQL - Procedure levantamento diario.sql
+++ 3-PostgreSQL - Procedure levantamento diario.sql
@@ -13,7 +13,7 @@ INSERT INTO estoque (nome_produto, quantidade, preco) VALUES
 SELECT * FROM estoque;
 
 --criando a procedure 
-CREATE or REPLACE PROCEDURe novas_compras (quantidade_adquirida INT)
+CREATE or REPLACE PROCEDURE novas_compras (quantidade_adquirida INT)
 LANGUAGE PLPGSQL
 as $$
 BEGIN
@@ -28,3 +28,41 @@ CALL novas_compras(100);
 
 SELECT * FROM estoque;
 
+
+
+
+###############################
+#                             #
+#        CÓDIGO COMENTADO     #
+#                             #
+###############################
+
+CREATE table estoque (
+  id serial PRIMARY KEY,
+  nome_produto varchar(50),
+  quantidade INT,
+  preco DECIMAL
+  )
+  
+INSERT INTO estoque (nome_produto, quantidade, preco) VALUES
+('TV', 10, 2700.00),
+('Refrigerador', 10, 3800.00),
+('Cooktop', 10, 599.00)
+
+SELECT * FROM estoque;
+
+--criando a procedure 
+CREATE or REPLACE PROCEDURE novas_compras (quantidade_adquirida INT)
+LANGUAGE PLPGSQL
+as $$
+BEGIN
+
+	UPDATE estoque --Atualizaremos a tabela estoque
+    set quantidade = quantidade + quantidade_adquirida  --na coluna quantidade, vamos pegar a quantidade existente no estoque e somar com a quantidade que acabou de chegar para o estoque da loja.
+    WHERE nome_produto = 'TV'; --na linha onde o nome do produto for igual a 'TV'
+
+end $$;
+
+CALL novas_compras(100); --chamamos a procedure e passamos a nova quantidade adquirida para a loja para seu estoque no caso 100. esses 100 se somara ao estoque que a loja já tinha.
+
+SELECT * FROM estoque; -- realizamos uma consulta da tabela estoque para ver a nova quantidade já inserida no estoque.
